aac9048187db6f4ef136ef1d894612d50dee1fb9
W6Matt <75140430+W6Matt@users.noreply.github.com>
2023-07-23 18:31:13 -0600

    Fixed Module 5

--- Z_Scratch_pad.ps1
+++ Z_Scratch_pad.ps1
@@ -1,33 +1,7 @@
-npm uninstall mongoose
-npm uninstall seedgoose
 
-npm install -g express-generator@4.16.1
-
-express --view=hbs --git --force
-
-npm install
-
-SET DEBUG=travlr:*
-npm start
-
-npm install mongoose@5.9.2
-
-npm install seedgoose@2.0.2
-
-./node_modules/.bin/seedgoose seed
-
-npm install -g @angular/cli@v6-lts
-
-ng new travlr-admin --defaults=true --skipGit=true --skipTests=true --directory app_admin
 
 cd .\app_admin
 
 ng serve
 
-ng generate component trip-listing
 
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"crossorigin="anonymous"></script>
